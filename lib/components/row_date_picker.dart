77242980edc8f4da6da16b7ee1e9cb43267b812b
zaknafein83 <franksisca@gmail.com>
2019-11-13 11:20:30 +0100

    primo split-code

--- lib/components/row_date_picker.dart
+++ lib/components/row_date_picker.dart
@@ -1,33 +1,20 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:template_prj/components/row_date_picker.dart';
 
-void main() => runApp(MyApp());
+class RowDatePicker extends StatefulWidget {
+      RowDatePicker({Key key, }) : super(key: key);
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: RowDatePicker(),
-    );
-  }
+    DateTime dateTime = DateTime.now();
+
+DateTime getDateTime(){
+  return dateTime;
 }
-/*
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyDatePicker createState() => _MyDatePicker();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyDatePicker extends State<RowDatePicker> {
 
   DateTime _dateTime = DateTime.now();
 
@@ -50,7 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
     }
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("widget.title"),
       ),
       body: Column(            
         mainAxisAlignment: MainAxisAlignment.center,
@@ -70,4 +57,4 @@ class _MyHomePageState extends State<MyHomePage> {
         ]),
       ); 
   }
-}*/
+}
